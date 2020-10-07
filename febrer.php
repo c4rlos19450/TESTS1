02bfd5f9ac4c24583c46c138896333b92254b556
carlos <carlosexpuig@gmail.com>
2020-10-07 12:52:48 +0200

    Revert "Revert "This reverts commit ba2cda40bbbe43669c5e0330e15b0740d297dc55.""
    This reverts commit abf7cf1891857d1771cc9a09a3c584e4c008a634.

--- febrer.php
+++ febrer.php
@@ -1,3 +1,4 @@
 echo 'pel febrer, carnestoltes el primer';
 echo 'Pel febrer flor a l'ametler';
 echo 'Mes tard o més matiner carnaval dins el febrer';
+echo 'Febrer,Febreret,set capes i un barret';
