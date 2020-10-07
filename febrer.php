0fe404a6c8aa458f232fb85f722269a083b18cab
carlos <carlosexpuig@gmail.com>
2020-10-07 13:31:54 +0200

    One more

--- febrer.php
+++ febrer.php
@@ -2,3 +2,4 @@ echo 'pel febrer, carnestoltes el primer';
 echo 'Pel febrer flor a l'ametler';
 echo 'Mes tard o més matiner carnaval dins el febrer';
 Es divdnres!!!!!
+echo 'Has de llaurar al Febrer el que al marc vulgues sembrar';
