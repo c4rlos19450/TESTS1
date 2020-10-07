a37e63a8961eaea513edba6a967db8024456e6a9
carlos <carlosexpuig@gmail.com>
2020-10-07 12:26:43 +0200

    Revert "Revert "Revert "Revert "Revert "Revert "New line febrer""""""
    This reverts commit 99755bb01347b6f28376d1a7aed150145f182472.

--- febrer.php
+++ febrer.php
@@ -1,5 +1,3 @@
 echo 'pel febrer, carnestotltes el primer';
 echo 'Pel febrer flor a l'amatller';
 echo 'Més tard o més matiner carnaval dins el febrer';
-'Febrer,Febreret,set capes i un barret';
-'Febrer,Febreret,set capes i un barret';
